9cf2f10cb00a33066a4a32048cc95d94579e55d2
Danilopc <sr_danilo@hotmail.com>
2022-12-05 17:40:14 +0100

    ditto's backcard

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+
 function resetGame() {
     document.location.reload();
 }
-
 const pokeAPIBaseurl = 'http://pokeapi.co/api/v2/pokemon/';
-
+const spritePokemon = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png";
 function loadPokemon (){
 fetch(pokeAPIBaseurl )
     .then( response => response.json())
     .then((data) => {console.log(data)
-    const selectPokemon =  data['name']
+    const selectPokemon =  data['results']
         console.log(selectPokemon)
 })
 }
+
+
+
+const  cards = document.querySelector('.cards')
+
+fetch(spritePokemon)
+.then((data) => {console.log(data)});
+
+
 
 
 loadPokemon()
