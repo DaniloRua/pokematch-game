83c306586058e3f3c9f33c302d98ac6429de6b4f
Danilopc <sr_danilo@hotmail.com>
2022-12-06 22:16:55 +0100

    add classlist

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
+const  cards = document.querySelector('.cards')
+const pokeAPIBaseurl = 'http://pokeapi.co/api/v2/pokemon/';
+const spritePokemon = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png";
 
 function resetGame() {
     document.location.reload();
 }
-const pokeAPIBaseurl = 'http://pokeapi.co/api/v2/pokemon/';
-const spritePokemon = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png";
+
 function loadPokemon (){
 fetch(pokeAPIBaseurl )
     .then( response => response.json())
     .then((data) => {console.log(data)
-    const selectPokemon =  data['results']
+    const selectPokemon = data['results']
         console.log(selectPokemon)
 })
 }
 
+loadPokemon()
 
 
-const  cards = document.querySelector('.cards')
 
 fetch(spritePokemon)
 .then((data) => {console.log(data)});
 
 
+function flip(){
+    cards.classList.toggle('cards-back');
+    turnBack();
+}
+
+function turnBack(){   
+    console.log('turning back'); 
+    setTimeout(() => {cards.classList.toggle('cards-back')},3000)
+    
+}
+
+function stopToggle(f){
+    clearTimeout(f)
+}
 
 
-loadPokemon()
