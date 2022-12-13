81a005bf217c76ef98b8a2ac81d2e56dadddbca3
Danilopc <sr_danilo@hotmail.com>
2022-12-13 19:16:47 +0100

    display img with foreach

--- app.js
+++ app.js
@@ -1,43 +1,107 @@
-const PokeAPI = "https://pokeapi.co/api/v2/pokemon/1"
+const PokeAPI = "https://pokeapi.co/api/v2/pokemon/"
 const gameBoard = document.getElementById('game');
 let cards = document.querySelectorAll('.cards');
-const card1 = document.getElementById('c1');
+
+
 
 // flip cards
 cards.forEach(function(card){
     card.addEventListener('click',function(){
-        card.classList.toggle('cards-back'),
+        card.classList.toggle('cards-back');
+        
         setTimeout(() => {card.classList.toggle('cards-back')},3000)
     })
-})
-
-//mixing the cards every reload
-cards.forEach(function(card) { 
-    let mixing = Math.floor(Math.random() * 12)
-    card.style.order = mixing
 });
 
 
-// name and sprites
+
+//name and sprites
 const pokemonPromises = fetch(PokeAPI).then((data) => data.json())
-.then((pokemonPromises) => console.log(pokemonPromises));
+.then((pokemonPromises) => {
+
+ console.log(pokemonPromises); 
+const promises = pokemonPromises.results.slice(0,-14)
+let doublePromises = promises.concat(promises)
 
 
-// sprites test
-fetch(spritePokemon)
-.then((res) => res.json())
-.then((data) => console.log(data))
+ console.log(doublePromises)
+ 
+
+    cards.forEach((card,index) => {
+    
+    const pokemon = doublePromises[index]
+    console.log(pokemon)
+    let pokeName = document.createElement('p');
+    pokeName.setAttribute('id',`${pokemon.name}`);
+    card.appendChild(pokeName).style= "color: white";
+    pokeName.innerHTML = `${pokemon.name}`;
+    
+    console.log(pokemon.url)
+    
+    const pokemonUnitsUrl = pokemon['url']
+
+    const Sprites = fetch(pokemonUnitsUrl).then((data) => data.json()).then((sprites) => { 
+        
+        const  spritesUnits = sprites['sprites']
+        console.log(spritesUnits['front_default']);
+        
+        const pokemonImg = document.createElement('img');
+
+        pokemonImg.setAttribute('src',`${spritesUnits['front_default']}`);
+        
+        card.appendChild(pokemonImg)
+    
+    
+
+    })
+
+
+})
+})
 
 
 
 
 
-const displayPromises = pokemonPromises => {
-    return `
-    <div class="card">
-        <h2>${pokemonPromises.name} </h2>
-    </div>`
-}
+
+      
+    
+  
+    //mixing the cards every reload
+        cards.forEach(function(card) { 
+        let mixing = Math.floor(Math.random() * 12)
+        card.style.order = mixing
+        //
+    });
+     
+    
+    
+    
+     
+   
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 
 //Display... test
 function ElementFromHtml(html) {
@@ -49,23 +113,13 @@ const decoy = ElementFromHtml(`
 <h2>${pokemonPromises.name}</h2>
 `)
 
-
-
-//generate random number
-const randomFaceCard = function () {
-   let mix = Math.floor(Math.random() *151)
-   console.log(mix);
-   return mix
-   
-}
- 
-
-
+*/
+/*
 // fazer uma funcao random para concatenar no link de download de sprites
 //jogar os valores em um array 
 // designar cada valor do array para 6 cards e duplicar
 //utilizar a funca random para sortear a posicao de cada pokemon
-
+*/
 // reload the game
 function resetGame() {
     document.location.reload();
@@ -75,6 +129,3 @@ function resetGame() {
 function stopToggle(f){
     clearTimeout(f)
 }
-
-//background-image: url("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png");
-
