3ea6ce8a36ff630d478e45dab043f4e1bdaa3724
Danilopc <sr_danilo@hotmail.com>
2022-12-14 17:55:56 +0100

    pokemon reveal

--- app.js
+++ app.js
@@ -4,91 +4,78 @@ let cards = document.querySelectorAll('.cards');
 
 
 
-// flip cards
-cards.forEach(function(card){
-    card.addEventListener('click',function(){
-        card.classList.toggle('cards-back');
+
+//name and sprites
+const pokemonPromises = fetch(PokeAPI).then((data) => data.json())
+    .then((pokemonPromises) => {
+
+        console.log(pokemonPromises);
+        const promises = pokemonPromises.results.slice(0, -14)
+        let doublePromises = promises.concat(promises)
+        console.log(doublePromises)
+        cards.forEach((card, index) => {
+
+            const pokemon = doublePromises[index]
+            let pokeName = document.createElement('p');
+            pokeName.setAttribute('id', `${pokemon.name}`);
+            card.appendChild(pokeName).style = "margin: 1vh; color: white";
+            pokeName.innerHTML = `${pokemon.name}`
+            const pokemonUnitsUrl = pokemon['url']
+            fetch(pokemonUnitsUrl).then((data) => data.json()).then(function sprites(sprites) {
+                const spritesUnits = sprites['sprites']
+                const pokemonImg = document.createElement('img');
+                pokemonImg.setAttribute('src', `${spritesUnits['front_default']}`);
+                pokemonImg.setAttribute('id', 'frontCard');
+                card.appendChild(pokemonImg).style = "width:9em ;background-size: contain;"
+
+                card.addEventListener('click', function () {
+
+                    card.classList.toggle('cards-back');
+                    pokemonImg.classList.toggle('sprite');
+                    pokeName.classList.toggle('sprite');
+                    setTimeout(() => { card.classList.toggle('cards-back'); pokemonImg.classList.toggle('sprite');pokeName.classList.toggle('sprite');
+
+                }, 2000)
+                })
+            })
+    
+
+        })
         
-        setTimeout(() => {card.classList.toggle('cards-back')},3000)
+
+        
     })
+
+
+
+// flip cards
+
+
+
+
+
+
+
+
+
+
+//mixing the cards every reload
+cards.forEach(function (card) {
+    let mixing = Math.floor(Math.random() * 12)
+    card.style.order = mixing
+    //
 });
 
 
 
-//name and sprites
-const pokemonPromises = fetch(PokeAPI).then((data) => data.json())
-.then((pokemonPromises) => {
-
- console.log(pokemonPromises); 
-const promises = pokemonPromises.results.slice(0,-14)
-let doublePromises = promises.concat(promises)
-
-
- console.log(doublePromises)
- 
-
-    cards.forEach((card,index) => {
-    
-    const pokemon = doublePromises[index]
-    console.log(pokemon)
-    let pokeName = document.createElement('p');
-    pokeName.setAttribute('id',`${pokemon.name}`);
-    card.appendChild(pokeName).style= " margin: 1vh; color: white;";
-    pokeName.innerHTML = `${pokemon.name}`;
-
-
-    console.log(pokeName);
-    
-
-
-
-
-
-    const pokemonUnitsUrl = pokemon['url']
-
-    const Sprites = fetch(pokemonUnitsUrl).then((data) => data.json()).then((sprites) => { 
-        
-        const  spritesUnits = sprites['sprites']
-       
-        const pokemonImg = document.createElement('img');
-
-        pokemonImg.setAttribute('src',`${spritesUnits['front_default']}`);
-        
-        card.appendChild(pokemonImg).style="width:9em ;background-size: contain; background"
-    
-    
-
-    })
-
-
-})
-})
 
 
 
 
 
 
-      
-    
-  
-    //mixing the cards every reload
-        cards.forEach(function(card) { 
-        let mixing = Math.floor(Math.random() * 12)
-        card.style.order = mixing
-        //
-    });
-     
-    
-    
-    
-     
-   
 
 
-
-    
-    
 
 
 
@@ -102,6 +89,6 @@ function resetGame() {
 }
 
 
-function stopToggle(f){
+function stopToggle(f) {
     clearTimeout(f)
 }
