a7f66fd273bf6679a9157d9cfe9be541c776a141
Danilopc <sr_danilo@hotmail.com>
2022-12-24 15:47:15 +0100

    commented

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
+//setting the variables
 const PokeAPI = "https://pokeapi.co/api/v2/pokemon/"
 const gameBoard = document.getElementById('game');
 let cards = document.querySelectorAll('.cards');
-
 let card1;
 let card2;
 let pairs = 0;
 let img1
 let name1;
-
-//name and sprites
+//get the names and sprites from Pokemon API
 const pokemonPromises = fetch(PokeAPI).then((data) => data.json())
     .then((pokemonPromises) => {
-
         console.log(pokemonPromises);
-        const promises = pokemonPromises.results.slice(0, -14)
-        let doublePromises = promises.concat(promises)
-        console.log(doublePromises)
-        
+        const promises = pokemonPromises.results.slice(0, -14) //selecting the number pokemon
+        let doublePromises = promises.concat(promises) // dubble to make match
+        // create a loop to put sprites ans names on each div
         cards.forEach((card, index) => {
             const pokemon = doublePromises[index]
             let pokeName = document.createElement('p');
@@ -30,97 +27,67 @@ const pokemonPromises = fetch(PokeAPI).then((data) => data.json())
                 pokemonImg.setAttribute('src', `${spritesUnits['front_default']}`);
                 pokemonImg.setAttribute('id', 'imgCard');
                 card.appendChild(pokemonImg).style = "width:10em;height:10em;margin-top:0px"
-
-                
-                 // flip cards
-+
-                card.addEventListener('click', function () {
-
-               
-                    if ( !card1 && !card2 ) {
-
-                        card1 = card;                
-                        card.classList.toggle('cards-back');
-                        img1 =pokemonImg
-                        pokemonImg.classList.toggle('sprite');
-                        name1 =  pokeName
-                        pokeName.classList.toggle('sprite');
-                       
-                    } else if ( card1 && !card2 ) {
-                     
-                        card2 = card; 
-
-                        card.classList.toggle('cards-back');
-                        pokemonImg.classList.toggle('sprite');
-                        pokeName.classList.toggle('sprite');
-                       
-                        
-                        if (card1.id === card2.id){
-                       console.log(card1.id)
-                        console.log(card1)
-                        console.log(card2)
-                            card1.style.pointerEvents='none';
-                            card2.style.pointerEvents='none';
-                            card1= null;
-                            card2= null;
-                            pairs += 1;
-                            console.log(pairs)
-                        }
-                            if(pairs >= 6){
-                               setTimeout(() => alert('GAME OVER! F5 to replay'), 2000)
-                            // open a modal window
-                        } else {
-                            console.log(card1)
-                            console.log(card2)
-                            setTimeout(() =>{
-                              
-                             
-
-
-                                card2.classList.add('cards');
-                                card2.classList.remove('cards-back');
-                                pokemonImg.classList.remove('sprite');
-                                pokeName.classList.remove('sprite');
-                              
-                                card1.classList.add('cards');
-                                card1.classList.remove('cards-back');
-                                pokemonImg.classList.remove('sprite');
-                                pokeName.classList.remove('sprite');
-                                   
-                                img1.classList.remove('sprite');
-                                name1.classList.remove('sprite');
+                    // flip cards and match them
+                    card.addEventListener('click', function () {
+                        if (!card1 && !card2) {
+                            card1 = card;
+                            card.classList.toggle('cards-back');
+                            img1 = pokemonImg
+                            pokemonImg.classList.toggle('sprite');
+                            name1 = pokeName
+                            pokeName.classList.toggle('sprite');
+                        } else if (card1 && !card2) {
+                            card2 = card;
+                            card.classList.toggle('cards-back');
+                            pokemonImg.classList.toggle('sprite');
+                            pokeName.classList.toggle('sprite');
+                            if (card1.id === card2.id) {
+                                console.log(card1.id)
+                                console.log(card1)
+                                console.log(card2)
+                                card1.style.pointerEvents = 'none';
+                                card2.style.pointerEvents = 'none';
                                 card1 = null;
                                 card2 = null;
-                                 
-                                
-                            },1500)
+                                pairs += 1;
+                                console.log(pairs)
+                            }
+                            if (pairs >= 6) {
+                                setTimeout(() => alert('GAME OVER! F5 to replay'), 2000)
+                                // open a modal window(next update)
+                            } else {
+                                console.log(card1)
+                                console.log(card2)
+                                setTimeout(() => {
+                                    card2.classList.add('cards');
+                                    card2.classList.remove('cards-back');
+                                    pokemonImg.classList.remove('sprite');
+                                    pokeName.classList.remove('sprite');
+                                    card1.classList.add('cards');
+                                    card1.classList.remove('cards-back');
+                                    pokemonImg.classList.remove('sprite');
+                                    pokeName.classList.remove('sprite');
+                                    img1.classList.remove('sprite');
+                                    name1.classList.remove('sprite');
+                                    card1 = null;
+                                    card2 = null;
+                                }, 1500)
+                            }
                         }
-                    }
-                })
+                    })
 
             })
         })
     })
 
-
-
-
-
-
-
 //mixing the cards every reload
-/*
 cards.forEach(function (card) {
     let mixing = Math.floor(Math.random() * 12)
     card.style.order = mixing
     //
 });
 
-*/
-
-
-// reload the game
+//reload the game
 function resetGame() {
     document.location.reload();
 }
-
